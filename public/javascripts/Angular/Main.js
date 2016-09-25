58caf8e726c79cdcc2d67a87b63f98a94d57cd9e
rahil vora <rahilvora@rahils-MacBook-Pro.local>
2016-09-25 05:25:00 -0700

    Adds URL button

--- public/javascripts/Angular/Main.js
+++ public/javascripts/Angular/Main.js
@@ -5,13 +5,10 @@
 var policyLinksApp = angular.module("policyLinksApp",["ngRoute"]);
 
 policyLinksApp.controller("LinkController",["$scope","$http","$location", function($scope, $http, $location){
-    // $http.get('api/getForm').success(function (result) {
-    //     debugger;
-    //     console.log(result);
-    // });
-    // Simple GET request example:
+
     $scope.states = [];
     $scope.region = "";
+    $scope.usl = "";
     $http({
         method: 'GET',
         url: '/api/getForm'
@@ -31,7 +28,6 @@ policyLinksApp.controller("LinkController",["$scope","$http","$location", functi
         // or server returns response with an error status.
     });
     $scope.saveRegion = function(){
-        debugger;
         $scope.region = $scope.selectedValue.split(" ")[0];
         $location.path('/selectorPage');
     };
@@ -43,11 +39,44 @@ policyLinksApp.controller("LinkController",["$scope","$http","$location", functi
         }
         baseURL +=$scope.region;
         console.log(baseURL);
-        // $http.post({
-        //     method:'GET',
-        //     url: 'api/generateURL'
-        // }).then(function successCallback(response){}, function errorCallBack(response){});
-        // console.log($scope.form);
+    };
+    function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(method, url, true);
+
+        } else if (typeof XDomainRequest != "undefined") {
+
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+
+        } else {
+
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+
+        }
+        return xhr;
+    }
+    $scope.generateURL3 = function(){
+        var baseURL = "http://107.170.124.232/export/url?url=http://nationalequityatlas.org/indicators/";
+        if($scope.form.economicbenefits == "Income Gains with Racial Equity"){
+            baseURL += "Income_gains_with_racial_equity/";
+        }
+        if($scope.form.selectedValue1 == "Income")
+            baseURL += "Income:32771/";
+        if($scope.form.selectedValue1 == "Income gains")
+            baseURL += "Income_gains:32776/";
+        if($scope.form.selectedValue1 == "Source of gains")
+            baseURL += "Source_of_gains:32781/";
+        baseURL +=$scope.region;
+        console.log(baseURL);
+        $scope.url = baseURL;
     };
 }]);
 
